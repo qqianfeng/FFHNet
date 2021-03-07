908b24d3687a4d2dfaac514792a54c6afdba7024
v.mayer <vincent.mayer@agile-robots.com>
2021-03-07 16:04:31 +0100

    chore: train config

--- config/train_config.py
+++ config/train_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60db7f3d5e7548d7ba34eaf61638e6ffb7df11f4f743ba4ea9e692c84b623d33
-size 1143
+oid sha256:8ce66da1bb1b65dae2ce2b94e7c819725ebdcd9ea2222512331c343c9fd0db27
+size 1703
