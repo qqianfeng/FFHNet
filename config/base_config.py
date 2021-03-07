3a30e8127841b08265b01859a19989185638ffa6
v.mayer <vincent.mayer@agile-robots.com>
2021-03-07 08:25:20 +0100

    feat: add train and base config

---
+++ config/base_config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49f929955b64ed7c35316f55088c2251a628f35a903e8cc5e3b3f2ae68bde9ab
+size 865
---
+++ config/train_config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60db7f3d5e7548d7ba34eaf61638e6ffb7df11f4f743ba4ea9e692c84b623d33
+size 1143
