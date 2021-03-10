eafa77c6a44231aaca7e128bbbe30c2a4b685bc4
v.mayer <vincent.mayer@agile-robots.com>
2021-03-10 10:31:50 +0100

    feat: visualize restored grasps

--- hithand_grabnet/utils/utils.py
+++ hithand_grabnet/utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5465b14fe86dd3da01774b18766126e1ba6542ea0650cf03292f87b97b5cfa7
-size 400
+oid sha256:3268535798822afb8efc66c6864c012becddd1c1c893634fafc2d07d277a565e
+size 1737
