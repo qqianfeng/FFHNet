b77d54d6f2ef4f59a3e8cd84f8a538e96f5783aa
v.mayer <vincent.mayer@agile-robots.com>
2021-03-10 19:43:26 +0100

    feat: define loss

--- hithand_grabnet/models/networks.py
+++ hithand_grabnet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af9668e352d7c82d6bf23b33d009663f4366b6ed0142e68b1cb38489ff9dd54b
-size 6652
+oid sha256:44fe0d7e8df5bc62dace1dd97ac109d16cdbbf83557f97e394ff6806c4a36218
+size 7160
