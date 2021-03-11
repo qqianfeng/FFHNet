5443bccd1e1c3ecdb7c8aac9cf0101e318e7a380
v.mayer <vincent.mayer@agile-robots.com>
2021-03-11 19:37:44 +0100

    fix: net input

--- hithand_grabnet/models/networks.py
+++ hithand_grabnet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44fe0d7e8df5bc62dace1dd97ac109d16cdbbf83557f97e394ff6806c4a36218
-size 7160
+oid sha256:ad425b34928101f4243c608034f0d0cec1b233ed316199037458c9890a48131d
+size 7240
