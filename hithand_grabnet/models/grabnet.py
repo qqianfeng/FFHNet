a82e13750d22edbb5b496d4ef1503e0310de8171
v.mayer <vincent.mayer@agile-robots.com>
2021-03-12 18:45:08 +0100

    feat: compute loss

--- hithand_grabnet/models/grabnet.py
+++ hithand_grabnet/models/grabnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c6acdc4283ad9739992fa75bf85ef3a61dcdbbbacd18b5acf8850867e4a33e0
-size 2629
+oid sha256:31fe03e25d2baa97ed4163c84def1939f4b0d94662381ef4d5a271435f2f41ed
+size 2690
