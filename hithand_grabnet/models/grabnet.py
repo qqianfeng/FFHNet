9f5188781ca9daf65e438a0d22c3cb18fbce4f07
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:54:22 +0100

    feat: save model

--- hithand_grabnet/models/grabnet.py
+++ hithand_grabnet/models/grabnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31fe03e25d2baa97ed4163c84def1939f4b0d94662381ef4d5a271435f2f41ed
-size 2690
+oid sha256:97868b0cc913c98a8340dd4c8adb73937b24b1123d733dadd20e7e6336574faa
+size 3699
