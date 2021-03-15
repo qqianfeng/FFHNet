84f7509b26f355f662e49592b4a90f3ab74974d2
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:53:11 +0100

    chore: change function order

--- hithand_grabnet/utils/utils.py
+++ hithand_grabnet/utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06b237cb80a73bc397d78146fd7eee1d3a9a94cfed7b514ec05fb6494f89592a
-size 5007
+oid sha256:b662efa9b2dfaf59913cfa60b10e162dcb1d62409c37c3974a3fca61efb1ec58
+size 5000
