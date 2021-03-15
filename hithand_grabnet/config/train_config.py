5f435abf8fa73b5d6a5a8ba40c8d19ed8d2752ea
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:52:21 +0100

    style: make training=True unchangeable

--- hithand_grabnet/config/train_config.py
+++ hithand_grabnet/config/train_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fc5709ab499a6cc40efe6ae2a5c87f43f4f1c226aa75d8dcbb11a9d811613fd
-size 3436
+oid sha256:02a204be9a77cc4eea7673e3607adb7da79fc0c1dfe138e1ee3c5c41e59f7a6d
+size 3242
