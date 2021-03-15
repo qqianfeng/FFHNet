b5c9af91f79d61626cff2ce4b90d7c50fc1eca73
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 13:47:23 +0100

    feat: add eval logic to train loop

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ca36601bd9c2eb8f072e387e575fdbeec39aa515f0d8d9ce38c416fb67fc66a
-size 2277
+oid sha256:76d26721e35cf447cc67eddaa440c2c321e91cb2f5ececd98ed4f4d5a326657c
+size 2718
