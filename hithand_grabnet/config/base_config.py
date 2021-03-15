fedfa73bf33ae770b443e926ae629fb5f5907c0c
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 13:47:56 +0100

    feat: add start_epoch to resume training

--- hithand_grabnet/config/base_config.py
+++ hithand_grabnet/config/base_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d8b07ab91f55713a2250efb21db0d1965104574c39dad8f5094a218897dd48d
-size 3120
+oid sha256:f3d1d17d68dd2f2fc7637a1843fa906b6406b0eeb7d3156ba163f3a60dc794dc
+size 3357
