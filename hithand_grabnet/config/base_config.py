cd6a691538cc719946426dd1f6e886c6856c371d
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:51:52 +0100

    feat: save config when parsing

--- hithand_grabnet/config/base_config.py
+++ hithand_grabnet/config/base_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:336702d6e1d3ef273e8b383a763da5ebfe7ee09b7ecca4f0c754899c790a1186
-size 1440
+oid sha256:4d8b07ab91f55713a2250efb21db0d1965104574c39dad8f5094a218897dd48d
+size 3120
