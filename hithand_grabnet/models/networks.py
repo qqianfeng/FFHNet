e7540ceb3432d2b6a2fb96e08e5425ca95e47d00
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:53:37 +0100

    fix: various typos

--- hithand_grabnet/models/networks.py
+++ hithand_grabnet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52930c070d27d15eef49147c7a2881b033a189eebd5a8d09ffbd879be2b8bd9f
-size 7511
+oid sha256:c5f814c3cda89fbae93844f0b2b53e6bcae6c80c4ae7019204b46df74f158866
+size 7478
