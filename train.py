41eedea30767c1f6c46c8cd2fe4e34d9fb87a144
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:50:54 +0100

    feat: add training logic

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7a618193b4526118ba72ab34873a3bd48d8e3c9607f946fd8298f984839fc9
-size 1540
+oid sha256:1ca36601bd9c2eb8f072e387e575fdbeec39aa515f0d8d9ce38c416fb67fc66a
+size 2277
