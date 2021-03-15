353f2a9db3ba87a85c5831642c62197dab809a3c
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 07:52:35 +0100

    fix: various writer bugs

--- hithand_grabnet/utils/writer.py
+++ hithand_grabnet/utils/writer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4761f53902739b2d04a4f956658fd7d0e773f9b203c2657405895ff3456d73f3
-size 3455
+oid sha256:a649ee26b78dcf9e67f53f60d5b7ead88e28e7bbe80c8d158a1652a9451495ef
+size 3509
