40e71731fd8518b94c17afadc26fe08116527a15
v.mayer <vincent.mayer@agile-robots.com>
2021-03-15 13:47:06 +0100

    style: rename test to eval

---
+++ hithand_grabnet/config/eval_config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:158a185f82b5f3af11229cfc13f939fb960917df2b1026b957869e591af3a6c2
+size 568
--- hithand_grabnet/config/test_config.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:283358d44d34c36cdff0f1ece5ce423d0f9cf7e7a72ef2f09b6bc80161d08a8f
-size 364
