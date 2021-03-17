37e6eeb638da2133b57fe016b1f1630f0d470275
v.mayer <vincent.mayer@agile-robots.com>
2021-03-17 10:27:27 +0100

    fix: typo

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07440b092813d66badaf299fb8deb8c1ace4b9821d1735ad64ddea4d9d3e2d4a
-size 3060
+oid sha256:153d986bee02f2aaefe41985c98d6e385887de4b275d8eaa8e352c83eb9218eb
+size 3042
