0fb16cc3b91da83ad99fc3a2891ff07f0ccafc10
v.mayer <vincent.mayer@agile-robots.com>
2021-03-19 11:22:27 +0100

    feat: add kl regularization

--- hithand_grabnet/config/train_config.py
+++ hithand_grabnet/config/train_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ebda6ee679f22cc39a746ece259de22c653ad04d402007c9c19583c31821324
+oid sha256:5e58296f7734d5ba27518ce0a9fdc5ae46cf7383e04662c2b705da9365176e3b
 size 3657
--- hithand_grabnet/models/grabnet.py
+++ hithand_grabnet/models/grabnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:372828904fedff52caec62b37d24b19ab8dec69d50f0e75e55348d21629e9066
-size 5354
+oid sha256:8b4fc0a092ba26cde2be55d999376023c822d8b1ffe69ad50470b59b24ab7e6c
+size 5404
