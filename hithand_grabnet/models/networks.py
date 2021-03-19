08bccd7ffdf5acd1211ffd5762630fefe976b43e
v.mayer <vincent.mayer@agile-robots.com>
2021-03-19 16:27:29 +0100

    feat: sample grasps

--- hithand_grabnet/models/networks.py
+++ hithand_grabnet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2180fec14d086c6d7b9dead4cd7338b5bbb421433ebf634dd2b7ccfea885566d
-size 7515
+oid sha256:26987a778d8c7a0f4698ca2b57428de90cfa9d1cebe6189e5b14ef3a4285ae7d
+size 7800
