f27568b2ecbd2d2d54671f4262a750c79b28646a
v.mayer <vincent.mayer@agile-robots.com>
2021-03-19 16:59:17 +0100

    test: eval config

--- hithand_grabnet/config/eval_config.py
+++ hithand_grabnet/config/eval_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37d85ce667d397bff86bd132c5c0880483300c630b4774ebb1d1b8b4a17a7e1e
-size 546
+oid sha256:ab4c93dd9fa8a28a52df047ea8b7aba213268df9c2c93c9ef74a89eb237bd3b4
+size 606
