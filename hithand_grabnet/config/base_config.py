0e9e764c094d66922adf070881e6da8b5f374e45
v.mayer <vincent.mayer@agile-robots.com>
2021-03-20 17:42:33 +0100

    chore: add model load path option

--- hithand_grabnet/config/base_config.py
+++ hithand_grabnet/config/base_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee8e20bc94c6651c74b52319ad512d0fc8dbc369b4875801ed66faa41020ea36
-size 3582
+oid sha256:fb46861c76f84d1b638ef18d9b39d86a6a1bd7944e583409fc402637616e5b67
+size 3849
