879b89ee96668733c5b39cc5f1711e39e5bea606
v.mayer <vincent.mayer@agile-robots.com>
2021-03-22 14:37:58 +0100

    feat: timing of grasp sampling

--- hithand_grabnet/models/networks.py
+++ hithand_grabnet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6a4d6c5023eb916003ab921ec6ee5a77021d33eba530a59d88345b3d8c65688
-size 8051
+oid sha256:dd910c26abf9cdd30f8dd0211fadeaa72e9587b9c2197ac8c3fdddc81352ad5d
+size 8150
