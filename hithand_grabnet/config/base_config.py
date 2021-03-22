caf66b40f1d4a9ca4304ac1d55d7d14ae131cbaf
v.mayer <vincent.mayer@agile-robots.com>
2021-03-22 09:15:40 +0100

    feat: add sys.argv check for starting from ros launch

--- hithand_grabnet/config/base_config.py
+++ hithand_grabnet/config/base_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb46861c76f84d1b638ef18d9b39d86a6a1bd7944e583409fc402637616e5b67
-size 3849
+oid sha256:ba9a706dbdd1f55c94c63318c388ff2eb0e315e105c473227ea347733e8e6ca9
+size 4068
