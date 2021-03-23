2c1cf8ffe32ab1cf0414eab4f949fcd27707c47a
v.mayer <vincent.mayer@agile-robots.com>
2021-03-23 10:37:13 +0100

    chore: rename grasp data set to grasp sampler data set

--- hithand_grabnet/data/grasp_data_set.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9be6f1f011bdf30e5341c21ae0e2ef700aa5ac97acd3175626826d18f30461bd
-size 7000
---
+++ hithand_grabnet/data/grasp_sampler_data_set.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbae7a98492f5565f960ee02a2c62f2dd1175d84c206f4545b30d6624a4e042b
+size 6976
