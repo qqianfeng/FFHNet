e85f45991044d736e377c3e85251c372c32bbd8b
v.mayer <vincent.mayer@agile-robots.com>
2021-03-26 17:39:46 +0100

    feat: grasp success net dataloader

--- hithand_grabnet/data/grasp_success_data_set.py
+++ hithand_grabnet/data/grasp_success_data_set.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb2999d358da0940f22e6a17fe1e3a3af2c5adacb7ae61e4e9447e81bb4de18a
-size 3361
+oid sha256:962d13b4cd33e43d084ba9fd0394332b0da50e2172bac53e35f6870484b4b73a
+size 6042
