416a70c23f13d541bd2df2baa7c4d6cfebf05cfe
v.mayer <vincent.mayer@agile-robots.com>
2021-03-29 14:45:53 +0200

    chore: rename grasp sampling data into grasp generator data

---
+++ FFHNet/data/ffhgenerator_data_set.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f41dc7e860932e402ffe00f020bf7967b24ce7b991772b065ef4b7f9fd057a9
+size 7009
--- FFHNet/data/grasp_sampler_data_set.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56402bb1b93f03a525b40e8cd67787466692d2a83f3d734511441be22e138358
-size 6957
