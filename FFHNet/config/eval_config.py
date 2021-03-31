9697accfa24139e6a6a4fe315604afbd061a764f
v.mayer <vincent.mayer@agile-robots.com>
2021-03-31 07:14:52 +0200

    feat: add eval gen/eva variable

--- FFHNet/config/eval_config.py
+++ FFHNet/config/eval_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab4c93dd9fa8a28a52df047ea8b7aba213268df9c2c93c9ef74a89eb237bd3b4
-size 606
+oid sha256:a2cedea6794db7854b8f8bda5539d0e45e33871fa64451484c7409d220917931
+size 1064
