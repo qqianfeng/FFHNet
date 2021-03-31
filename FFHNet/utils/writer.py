59e3469e77b1ee054cffa71fcda23571c1853b83
v.mayer <vincent.mayer@agile-robots.com>
2021-03-31 07:16:03 +0200

    fix: have two functions for eval train instead of one

--- FFHNet/utils/writer.py
+++ FFHNet/utils/writer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0c4dae465b2a0f2a9bb1d2e3e1fdfc5dbfafba41d7359829a8c65701a1800fb
-size 4537
+oid sha256:90e239ed344a18580b8b05fc6235683cfde0f6a8b2998862f3d613f168b4d8a4
+size 4764
