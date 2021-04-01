e57f4b453804f292f8049574e41622682fa83869
v.mayer <vincent.mayer@agile-robots.com>
2021-04-01 15:39:33 +0200

    chore: rename some methods

--- FFHNet/models/networks.py
+++ FFHNet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3adfb0bddd8bc9da4abb62116ca086aa8afba0c8bda9b15f7da1274cf79cde4d
-size 10639
+oid sha256:fee48851150d751d0a42a341b087c11f7d93faa226a076c41bc2b90fc34fa3d5
+size 10641
