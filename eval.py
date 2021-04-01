8196b59eb82162aa0ba40b314d5767595205b114
v.mayer <vincent.mayer@agile-robots.com>
2021-04-01 15:36:07 +0200

    feat: eval eva accuracy

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8d8c9898b5d9a2adaa2e4003a05bc9c530be3246e019f22b24c62156ed49ed7
-size 3304
+oid sha256:30f7411e7b3b3ea34174f4ae84e54342c0798452c5ef7881eb132453293cf69e
+size 4895
