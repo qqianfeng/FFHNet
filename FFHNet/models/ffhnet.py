1549ea8d2c1e9ad2083df592f095264272643886
v.mayer <vincent.mayer@agile-robots.com>
2021-04-04 20:24:22 +0200

    feat: add refine grasps

--- FFHNet/models/ffhnet.py
+++ FFHNet/models/ffhnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecfd1caf64d4c4a9acf2bad6e29027c0efc9b778e274698c95ab6795fb6367e6
-size 13784
+oid sha256:83908e35cce15c1237bc52540e62001c235b14a0de399e1a5d10c8f6885318d2
+size 19818
