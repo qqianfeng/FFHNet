3959386c264275201e4bc2d9fd09c3d21d583a86
v.mayer <vincent.mayer@agile-robots.com>
2021-04-04 20:22:38 +0200

    feat: added eval grasp refinement

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49f4c7055cfb217a61d4bff0580c0c08c328ae03ac2194054c9cc63a91d57026
-size 4911
+oid sha256:390d0d47828e55c263f009f597ab7733c7cf4d55fecef1ae033adb49ccbd865b
+size 5359
