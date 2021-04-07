03e91932947f0a952c14e0384cd0d3f2ee06a1d5
v.mayer <vincent.mayer@agile-robots.com>
2021-04-07 09:37:43 +0200

    feat: refine grasps

--- FFHNet/config/eval_config.py
+++ FFHNet/config/eval_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:176f3e4cf3574be4893bd1ad9f9b8a127716b593199cb41ee3497bd6a21e1e08
-size 1065
+oid sha256:7aa5e463637febbdbfa1de59447330501dc3f337af738a097b0fe50ce0ed4e88
+size 1308
--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:390d0d47828e55c263f009f597ab7733c7cf4d55fecef1ae033adb49ccbd865b
-size 5359
+oid sha256:b30e5d53c034edb1f3adaf938810c0ba0c54d01253ae59d1a4b0d232bf31aaa1
+size 5556
