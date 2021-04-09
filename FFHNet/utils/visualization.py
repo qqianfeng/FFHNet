f1353313ac6916345e81508f1e178ab297269b1a
v.mayer <vincent.mayer@agile-robots.com>
2021-04-09 12:23:46 +0200

    feat: visualize camera

--- FFHNet/utils/visualization.py
+++ FFHNet/utils/visualization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04c0ce350e072d790d83fbc251b9df23ec8694c92902a9705446325f8936ba2e
-size 5228
+oid sha256:a3a4d7743597e7ba57882127f1ff4708abd799142a2c1a0190ff8231dee0cf45
+size 5572
