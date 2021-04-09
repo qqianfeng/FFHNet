8b23255c5297bd55d81bb0e0d76f5731e9cfee4b
v.mayer <vincent.mayer@agile-robots.com>
2021-04-09 12:25:21 +0200

    fix: point_clouds = pcd

--- FFHNet/data/ffhgenerator_data_set.py
+++ FFHNet/data/ffhgenerator_data_set.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:589ba6de2e20d489fb0e0a55241cbe70a69e12470d66040b728cd052cdb28fc4
-size 7130
+oid sha256:fa7dae0f34582bd6b3b0d940fb26c5bab716dbe457b65a4dd4e7e6f20f05ed39
+size 7169
