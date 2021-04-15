71f2367219c12d9adf7685bb22afdbe8d2140503
v.mayer <vincent.mayer@agile-robots.com>
2021-04-15 16:18:47 +0200

    chore: convert meshes default to stl

--- scripts/convert_meshes.py
+++ scripts/convert_meshes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e45ec886c6a6112d675895410f82eb1e9150c2ae175e211c15d2d2fb1cf9cfe1
-size 914
+oid sha256:2ab3e9234236cb1bd1eb9862470cad21042126202756d5ad1cab8a3b283ae015
+size 1103
