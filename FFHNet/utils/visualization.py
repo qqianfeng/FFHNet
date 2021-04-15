c62c1bc3ea2fa72d2a42d052c84ada889d0a13d9
v.mayer <vincent.mayer@agile-robots.com>
2021-04-15 16:19:51 +0200

    feat: visualize hand and object in pyrender

--- FFHNet/utils/visualization.py
+++ FFHNet/utils/visualization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c64e3dd8d1b239a08d46f25d3fc36698afcccff4ed9d4d62840f49ab8c8df7c6
-size 5599
+oid sha256:c5963d9031b6d3be242dea0b7557431680c6634b94ebdea54bccf03de1514976
+size 8718
