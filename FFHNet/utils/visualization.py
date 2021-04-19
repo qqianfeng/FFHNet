47ac143a46034d5744d946089e360f46c2457fe3
v.mayer <vincent.mayer@agile-robots.com>
2021-04-19 12:54:15 +0200

    feat: show pull joint config in pyrender for ground truth graps

--- FFHNet/utils/visualization.py
+++ FFHNet/utils/visualization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a2fd790b8dc3643fc6a67433b8e7eb4954c5057e932fa7022d205e549b200a8
-size 12404
+oid sha256:2c3ef1334c365ac562cb22dd55fce3de50f905d5d0d30df9685c5fb87ba590e4
+size 14892
