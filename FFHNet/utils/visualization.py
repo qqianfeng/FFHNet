62625797df1e0403294e45467b40785d886379ea
v.mayer <vincent.mayer@agile-robots.com>
2021-04-29 19:15:53 +0200

    feat: plot evaluator curve

--- FFHNet/utils/visualization.py
+++ FFHNet/utils/visualization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62901848e7f7a9ad7594f69dda57f1e7500ca5a0660e3207d1d5b12b0dcd2342
-size 19448
+oid sha256:49df006d0ec3b83676d51bb467fc17d65380976a2ecf8aec07b9b68e40ae1621
+size 21053
