cfed53552a4c569bcf0fa7f3cc562b83709c20c4
v.mayer <vincent.mayer@agile-robots.com>
2021-04-29 09:55:30 +0200

    feat: hard negative mining

--- FFHNet/utils/utils.py
+++ FFHNet/utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:986ab09ee1237ced21ea2e640c4da6a55ba7556ace4df6631926cf887072e43d
-size 10440
+oid sha256:8580c80a311e77ec16d33c6353d45f005113230d318fc4b23aa5173e1ed2fed7
+size 11312
