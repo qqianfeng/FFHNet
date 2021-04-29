6d267b2262af63b666a996ffaccce9e3fbfb84de
v.mayer <vincent.mayer@agile-robots.com>
2021-04-29 09:55:17 +0200

    feat: enable uniform sampling from generator

--- FFHNet/models/networks.py
+++ FFHNet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:effa01f03fd1272126b0c2d6b91472f23d1eda67bc750fda447c977371772927
-size 10556
+oid sha256:a8c33cf1f6f2e1c7d6d42c7aa7131ce0351146785aa2b7d629445a04c4b010be
+size 10668
