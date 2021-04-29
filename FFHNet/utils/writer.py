8e3cbe957aa87113ce4e7c239bd199aa215c4416
v.mayer <vincent.mayer@agile-robots.com>
2021-04-29 09:54:52 +0200

    fix: time display during training

--- FFHNet/utils/writer.py
+++ FFHNet/utils/writer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52cfa6ae23934023135c19504a8cb9c8da816b73038aa2908d449fd987bba878
-size 4762
+oid sha256:c3c12bf8e61abf42568863f340e23b4b286a7fc44b54b204738f3cbf52f30bbc
+size 4764
