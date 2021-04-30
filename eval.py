24ca630ffbb6d2d81cf96f30916c7143980bda06
v.mayer <vincent.mayer@agile-robots.com>
2021-04-30 17:38:37 +0200

    feat: evaluate eva accuracy by default

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71f1afca20137b6308b3cf6e576add73aca4d5979b770f7bc80b8c788e795466
-size 9870
+oid sha256:15d1ef404eca938ff8a9a4e62551fac8d8aeeda5efab2562196cff51f6c4eeb4
+size 10220
