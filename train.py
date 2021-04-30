ce4cdd0aecf9ac560b6179f316bddcba156131a5
v.mayer <vincent.mayer@agile-robots.com>
2021-04-30 11:13:41 +0200

    fix: training issues

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a51d9c30ce0ec7c9ca7c99f4611983c78f276b82a834add2693900d878786d96
-size 5612
+oid sha256:5aee049eca85bf10c51495b5fa49b30132f32f3f6a3883468aa86048d79fd0ad
+size 5856
