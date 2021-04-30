d08bde031ea62a2309de1d06f12ee7a4dbb8abc9
v.mayer <vincent.mayer@agile-robots.com>
2021-04-30 11:15:27 +0200

    move all meshes to mesh folder

--- scripts/convert_meshes.py
+++ scripts/convert_meshes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42f12c07d3c07287d6a0503e312e4aa0cc4e8f4e8f10ac7712aa547622f63a51
-size 982
+oid sha256:3735b1d7d1e161e95ae26a65b1985b657c9ddb23aaf28a329337a27b63897162
+size 819
