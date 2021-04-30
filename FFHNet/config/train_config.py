6cafb64212e37d8e2f218d864944b8d4659f1d93
v.mayer <vincent.mayer@agile-robots.com>
2021-04-30 17:38:51 +0200

    feat: introduce eval_frequency

--- FFHNet/config/train_config.py
+++ FFHNet/config/train_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:611e2f0c3ee0ad69a0e16de0223402de91c2e6212a6aec1aea0a47dc08e4ab5d
-size 4131
+oid sha256:b5ed05be161bcbb1b51172fd7b94ef9686a876e586cacde459a10b9756d33688
+size 4374
