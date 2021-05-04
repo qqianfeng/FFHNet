6a118be05b10142ddf643065051ea1c34d037e2d
v.mayer <vincent.mayer@agile-robots.com>
2021-05-04 17:50:35 +0200

    perf: remove 4th resblock

--- FFHNet/models/networks.py
+++ FFHNet/models/networks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e69618ce59233a3dcbf32ff6184e635e7f2f9dce2d78cf0b2f2c53b280088e11
-size 10810
+oid sha256:a8c33cf1f6f2e1c7d6d42c7aa7131ce0351146785aa2b7d629445a04c4b010be
+size 10668
