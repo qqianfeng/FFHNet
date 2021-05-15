7a3fd55b76148ee9a36ea283754752422ef4f5f8
v.mayer <vincent.mayer@agile-robots.com>
2021-05-15 17:11:53 +0200

    feat: visualize num removed grasps

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2954a41aa73dd16b4341922f666e7f78918fdc1290c5579731baeef940752a92
-size 16875
+oid sha256:c8548e552e377ff1489bba0d55c4685f3140ee656d621bcbfdceb71fcc6e6a6e
+size 19076
